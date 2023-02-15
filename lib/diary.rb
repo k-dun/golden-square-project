427f29d3c277d328a267279b945d93582bf259e4
Konrad <kdun.work@gmail.com>
2023-02-15 10:42:36 +0000

    Added next required test and wrote enough code to pass it.

--- lib/diary.rb
+++ lib/diary.rb
@@ -2,3 +2,7 @@ def make_snippet(str)
   snippet = str.split(" ")
   return snippet[0..4].join(" ") + "..."
 end
+
+def count_words(str)
+  return str.split(" ").size
+end
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -6,3 +6,10 @@ RSpec.describe 'make_snippet method' do
     expect(result).to eq "Makers might just be the..."
   end
 end
+
+RSpec.describe 'count_words method' do
+  it "counts words in the string" do
+    result = count_words("RSpec please don't disRSpect me.")
+    expect(result).to eq 5
+  end
+end
