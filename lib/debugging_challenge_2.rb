82d8152d266faef172e15dc7a1ffebd84d1609c6
Konrad <kdun.work@gmail.com>
2023-02-20 13:57:46 +0000

    Completed challenge for chapter 7.

---
+++ lib/debugging_challenge_2.rb
@@ -0,0 +1,29 @@
+class LetterCounter
+  def initialize(text)
+    @text = text
+  end
+
+  def calculate_most_common()
+    counter = Hash.new(0)
+    most_common = nil
+    most_common_count = 0
+    @text.chars.each do |char|
+      next if !(is_letter?(char))
+      counter[char] += 1
+      if counter[char] > most_common_count
+        most_common = char
+        most_common_count += 1
+      end
+    end
+    return [most_common_count, most_common]
+  end
+
+  private
+
+  def is_letter?(letter)
+    return letter =~ /[a-zA-Z]/
+  end
+end
+
+counter = LetterCounter.new("Digital Punk Digital Punk")
+p counter.calculate_most_common
